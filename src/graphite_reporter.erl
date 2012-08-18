6e47413e916d4832662136c3b7c7b8b8a96732ff
Derrick Schneider <derrick.schneider@gmail.com>
2012-08-18 14:04:35 -0700

    Fix bugs preventing graphite_reporter from sending correct data. Clean up utils a bit.

--- src/graphite_reporter.erl
+++ src/graphite_reporter.erl
@@ -12,7 +12,7 @@ init(_Config) -> {ok,ready}.
 gather_start(_) -> [].
 
 gather(MetricName,_MetricType,Data,Gatherer,_Config) when is_float(Data); is_integer(Data) ->
-	["metricsmaw." ++ MetricName ++ " " ++ utils:data_to_string(Data) ++ " " ++ integer_to_list(utils:unix_time()) ++ utils:newline() | Gatherer];
+	[utils:join(["metricsmaw." ++ MetricName,utils:data_to_string(Data),integer_to_list(utils:unix_time()),"\n"]," ") | Gatherer];
 	
 %% In this case, create a single line for each element in the list, which is assumed to be a list of {Key,Value} tuples
 gather(MetricName,MetricType,Data,Gatherer,Config) when is_list(Data) ->
@@ -28,13 +28,14 @@ gather(MetricName,MetricType,Data,Gatherer,Config) when is_list(Data) ->
 gather_end([],_Config) -> {ok,[]};
 gather_end(Gatherer,Config) ->
 	% ship it off to the graphite host
+	io:format("Sending Msg to Graphite ~p~n",[lists:flatten(Gatherer)]),
 	GraphiteHost = proplists:get_value(host,Config,"localhost"),
 	GraphitePort = proplists:get_value(port,Config,2003),
 	Timeout = proplists:get_value(timeout,Config,6000),
 	Response = gen_tcp:connect(GraphiteHost,GraphitePort,[list,{packet,0}],Timeout),
 	case Response of
 		{ok,Socket} ->
-			gen_tcp:send(Socket,list_to_binary(Gatherer)),
+			gen_tcp:send(Socket,lists:flatten(Gatherer)),
 			gen_tcp:close(Socket);
 		{error,Reason} ->
 			io:format("Could not send to graphite ~p~n",[Reason])
--- src/utils.erl
+++ src/utils.erl
@@ -11,25 +11,17 @@ join(List,Sep) ->
 join([],Acc,_Sep) -> Acc;
 
 % when accumulator is new (no sep at beginning)
-join([H|T],[] = Acc,Sep) when is_integer(H) ->     
-    join(T,Acc ++ integer_to_list(H),Sep);
+join([H|T],[] = Acc,Sep) ->     
+    join(T,Acc ++ data_to_string(H),Sep);
 
-% every other case
-join([H|T],Acc,Sep) when is_integer(H) ->
-	join(T,(Acc ++ Sep) ++ integer_to_list(H),Sep);
-	
-% float versions
-% when accumulator is new (no sep at beginning)
-join([H|T],[] = Acc,Sep) when is_float(H) ->     
-    join(T,Acc ++ float_to_list(H),Sep);
-
-% every other case
-join([H|T],Acc,Sep) when is_float(H) ->
-	join(T,(Acc ++ Sep) ++ float_to_list(H),Sep).
-	
+% every other non-start case
+join([H|T],Acc,Sep) ->
+	join(T,(Acc ++ Sep) ++ data_to_string(H),Sep).
+		
 % convert an int or a float to a string
 data_to_string(Value) when is_integer(Value) -> integer_to_list(Value);
-data_to_string(Value) when is_float(Value) -> float_to_list(Value).
+data_to_string(Value) when is_float(Value) -> float_to_list(Value);
+data_to_string(Value) when is_list(Value) -> Value.
 
 % return the number of seconds since the unix epoch
 unix_time() -> calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time( now()))-719528*24*3600.
